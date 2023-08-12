1aaef56d38d90d9b46c7b148e36e7d3a33c8529c
Qiyue Zhang <2108335219@qq.com>
2023-08-12 19:18:16 -0400

    1

--- JS/slz.js
+++ JS/slz.js
@@ -18,25 +18,6 @@ function His(){
 }
 
 
-var flag=true;
-
-function show_menu(){
-    var SmallMenu = document.getElementById("SmallMenu");
-    if(flag){
-        SmallMenu.style.display="block";
-        flag = false;
-    }else{
-        SmallMenu.style.display="none";
-        flag = true;
-    }
-}
-
-
-function show_SmallMenu(){
-    var SmallMenu = document.getElementById("SmallMenu");
-    SmallMenu.style.display="none";
-    flag = true;
-}
 
 
 function ti(){
