499581ea93f8ab72e295c53e2686e9d290feed77
L-Enoch2 <141799638+L-Enoch2@users.noreply.github.com>
2023-08-15 22:28:02 -0400

    1
    1

--- JS/slz.js
+++ JS/slz.js
@@ -28,17 +28,3 @@ function ti(){
 }
 
 
-function confirm(){
-    var a = document.getElementById("last").value
-    var b = document.getElementById("first").value
-    var c = document.getElementById("email").value
-
-
-    if(a.length > 0  && b.length > 0 && c.length > 0){
-        alert("the items have been added")
-    }else{
-        alert("please enter you info")
-    }
-  
-    
-}
