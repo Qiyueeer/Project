4e9db2e3001278a542b25ce44617ada2de23862d
L-Enoch2 <141799638+L-Enoch2@users.noreply.github.com>
2023-08-15 22:28:14 -0400

    1
    1

--- JS/slz.js
+++ JS/slz.js
@@ -27,4 +27,55 @@ function ti(){
     
 }
 
+<<<<<<< Updated upstream
 
+=======
+<<<<<<< HEAD
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+const searchResults = document.getElementById("searchResults");
+
+
+searchButton.addEventListener("click", performSearch);
+
+
+function performSearch() {
+
+    const keyword = searchInput.value;
+
+
+    displaySearchResults(keyword);
+}
+
+
+function displaySearchResults(keyword) {
+
+    searchResults.innerHTML = "";
+
+
+    const results = ["结果1", "结果2", "结果3"];
+
+
+    results.forEach(result => {
+        const resultElement = document.createElement("p");
+        resultElement.textContent = result;
+        searchResults.appendChild(resultElement);
+    });
+=======
+
+function confirm(){
+    var a = document.getElementById("last").value
+    var b = document.getElementById("first").value
+    var c = document.getElementById("email").value
+
+
+    if(a.length > 0  && b.length > 0 && c.length > 0){
+        alert("the items have been added")
+    }else{
+        alert("please enter you info")
+    }
+  
+    
+>>>>>>> 8698e332e03122976ef9bc3959f7e0bc26f2e2cf
+}
+>>>>>>> Stashed changes
