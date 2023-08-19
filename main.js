287d5590056911519a1a5365e5cf1167af274d66
Qiyue Zhang <2108335219@qq.com>
2023-08-18 21:24:05 -0400

    1

---
+++ main.js
@@ -0,0 +1,80 @@
+function Start(){
+    window.scrollTo(0, document.documentElement.scrollHeight-document.documentElement.clientHeight);
+}
+
+setInterval (function ti(){
+
+    let today = new Date()
+    document.getElementById("time").innerHTML = today
+    1000
+})
+
+function ti(){ 
+}
+
+
+
+var carts = new Array();
+function confirm(){
+    var a = document.getElementById("program").value;
+    var b = document.getElementById("count").value;
+    alert(a);
+alert(b);
+}
+
+function confirm(){
+    var b = document.getElementById("count").value;
+    counts.push('1'+b);
+ }
+ function viewCart(){
+     alert(counts[0]);
+ 
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
